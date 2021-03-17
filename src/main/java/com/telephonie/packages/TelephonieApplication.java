8fcd757b1345df56bda33577b9c6bae24f3bd2a6
hery_toavina <aherytoavina@gmail.com>
2021-03-17 21:45:46 +0300

    boot

---
+++ src/main/java/com/telephonie/packages/TelephonieApplication.java
@@ -0,0 +1,13 @@
+package com.telephonie.packages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephonieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelephonieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telephonie/packages/TelephonieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telephonie.packages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephonieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
