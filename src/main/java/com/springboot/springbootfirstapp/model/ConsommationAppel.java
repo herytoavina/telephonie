02b90aab4d222a99d3b75f6a635d119992171a95
hery_toavina <aherytoavina@gmail.com>
2021-04-01 23:43:11 +0300

    typeForfait

---
+++ src/main/java/com/springboot/springbootfirstapp/model/ConsommationAppel.java
@@ -0,0 +1,95 @@
+package com.springboot.springbootfirstapp.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsommationAppel {
+	
+	private String idForfait;
+	private int valeurAppel;
+	private int valeurAutre;
+	
+	public String getIdForfait() {
+		return idForfait;
+	}
+	public void setIdForfait(String idForfait) {
+		this.idForfait = idForfait;
+	}
+	public int getValeurAppel() {
+		return valeurAppel;
+	}
+	public void setValeurAppel(int valeurAppel) {
+		this.valeurAppel = valeurAppel;
+	}
+	public int getValeurAutre() {
+		return valeurAutre;
+	}
+	public void setValeurAutre(int valeurAutre) {
+		this.valeurAutre = valeurAutre;
+	}
+	
+	public ConsommationAppel() {
+		super();
+	}
+	
+	public ConsommationAppel(String idForfait, int valeurAppel, int valeurAutre) {
+		super();
+		this.idForfait = idForfait;
+		this.valeurAppel = valeurAppel;
+		this.valeurAutre = valeurAutre;
+	}
+	
+	public ConsommationAppel( int valeurAppel, int valeurAutre) {
+		super();
+		this.valeurAppel = valeurAppel;
+		this.valeurAutre = valeurAutre;
+	}
+	
+	private static final String INSERT_CONSOMMATION = "insert into consommationAppel (idForfait, valeur_appel, valeur_autre) values (?, ?, ?)";
+	public void insert() throws SQLException {
+		Connect c = new Connect();
+		Connection conn = null;
+		PreparedStatement statement = null;
+		try {		
+			conn = c.getConnection();
+			conn.setAutoCommit(false);
+			statement = conn.prepareStatement(INSERT_CONSOMMATION);
+			statement.setString(1, this.getIdForfait());
+			statement.setInt(2, this.getValeurAppel());
+			statement.setInt(3, this.getValeurAutre());
+			statement.execute();
+			conn.commit();
+		} catch (Exception ex) {
+			conn.rollback();
+			System.out.println(ex);
+		} finally {
+			if(statement != null) statement.close();
+			if(conn != null) conn.close();
+		}
+		
+	}
+	
+	private static final String ALL_CONSOMMATION = "select * from consommationAppel";
+	public List<ConsommationAppel> listeForfait(){
+		Connect con = new Connect();
+		List<ConsommationAppel> result = new ArrayList<ConsommationAppel>();
+		try {
+			Connection c = con.getConnection();
+			PreparedStatement preparedStatement = c.prepareStatement(ALL_CONSOMMATION);
+			ResultSet rs = preparedStatement.executeQuery();
+			while(rs.next()) {
+				result.add(new ConsommationAppel(rs.getString("idForfait"), rs.getInt("valeur_appel"), rs.getInt("valeur_autre")));
+			}
+			c.close();
+			
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		return result;
+	}
+	
+}
